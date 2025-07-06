710ad1644d7ec8b7456009d7c39cd85490b58d9d
Matteo <matteo2005.lammens@gmail.com>
2025-07-06 19:58:58 +0200

    Total rework of byte encoding/decoding and replacing encoder

--- Pendant/Pendant.ino
+++ Pendant/Pendant.ino
@@ -2,7 +2,7 @@
 Project: CNC Pendant, pendant side
 Description: Sends jog inputs from jog wheel and feed rates when feed buttons are pressed using serial communication.
 Author: Matteo https://github.com/BigLMatt
-Date last altered: 01/07/2025
+Date last altered: 06/07/2025
 
 Sender is arduino uno R4 wifi
 Hook up TX of sender to RX of receiver and also connect both GND to eachother
@@ -27,63 +27,76 @@ Encoder readout code based on code found here https://github.com/mo-thunderz/Rot
 #define feedSpeedPot A0
 
 volatile int readDelta = 0;
-volatile uint8_t old_AB = 0;
 int sumFeedSpeed = 0;
 int feedSpeed = 0;
 int speedAmount = 0;
+bool prevTimes10 = false;
+bool prevTimes100 = false;
+bool prevEnableY = false;
+bool prevEnableZ = false;
+bool prevPressed = false;
 
 unsigned long lastFeedTime = 0;
 const unsigned long feedInterval = 100;
+const unsigned long setupInterval = 2000;
+unsigned long lastSetupTime = 0;
 
-// Encode to save space in transmission
-uint8_t encodeDelta(int delta, bool times10, bool times100, bool Yaxis, bool Zaxis) {
+// Encode change in axis or multiplicand jog
+uint8_t encodeAxis(bool times10, bool times100, bool enableY, bool enableZ){
   uint8_t encoded = 0;
 
-  // Put sign bit at position 7
+  // Put factor 10/100 bits in postion 4 and 5 respectively
+  if(times10) encoded |= (0b01 << 4);
+  else if(times100) encoded |= (0b10 << 4);
+  
+  // Put axis direction bits at position 2 and 3
+  if(enableY) encoded |= (0b01 << 2);
+  else if(enableZ) encoded |= (0b10 << 2);
+  else encoded |= (0b00 << 3);
+
+  return encoded;
+}
+
+// Encode jog delta and direction
+uint8_t encodeDelta(int delta) {
+  uint8_t encoded = 0b01000000;
+
+  // Put sign bit at position 5
   if (delta < 0) {
-    encoded |= 0b10000000;
+    encoded |= 0b1 << 5;
     delta = -delta;
   }
 
-  // Limit max delta to 7
-  if (delta > 7){
-    delta = 7;
+  // Limit max delta to 31
+  if (delta > 31){
+    delta = 31;
     Serial.println("Delta too high");
   } 
 
-  // Put factor 10/100 bits in postion 5 and 6 respectively
-  if(times10) encoded |= (0b01 << 5);
-  else if(times100) encoded |= (0b10 << 5);
-  else  encoded |= (0b00 << 5);
-  
-  // Put axis direction bits at position 3 and 4
-  if(Yaxis) encoded |= (0b01 << 3);
-  else if(Zaxis) encoded |= (0b10 << 3);
-  else encoded |= (0b00 << 3);
-
   // Put the trimmed delta in the remaining spots
-  encoded |= (delta & 0b00000111);
+  encoded |= (delta & 0b00011111);
 
   return encoded;
 }
 
 uint8_t encodeFeed(bool feedFWD, bool feedREV, int speedVal){
-  uint8_t result = 0;
+  uint8_t encoded = 0b10000000;
 
-  speedVal = speedVal >> 3;
-  result |= (speedVal & 0b01111111);
+  speedVal = speedVal >> 4;
+  encoded |= (speedVal & 0b00111111);
 
-  if(feedREV && !feedFWD){
-    result |= 0b10000000;
-  } else if(!(!feedREV && feedFWD)){
-    return 0;
+  if(feedREV){
+    encoded |= 0b01000000;
+  } else if(feedFWD){
+    encoded |= 0b00000000;
   }
 
-  return result;
+  return encoded;
 }
 
 void encoderReadout() {
   static int8_t encVal = 0;   // Encoder value
+  static uint8_t old_AB = 3;  // Lookup table index (starts at 11)
   static const int8_t enc_states[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};  // Lookup table
 
   old_AB <<= 2;  // Shift left twice, remembering previous state
@@ -94,11 +107,11 @@ void encoderReadout() {
   encVal += enc_states[(old_AB & 0x0f)];
 
   // Update counter
-  if(encVal > 1){     // Could be 3 depending on construction of encoder
+  if(encVal > 3){     // Could be 1 depending on construction of encoder half or full quadrature
     readDelta++;   // Update counter
     encVal = 0;
   }
-  else if(encVal < -1){   // Could be -3 depending on construction of encoder
+  else if(encVal < -3){   // Could be -1 depending on construction of encoder half or full quadrature
     readDelta--;     // Update counter
     encVal = 0;
   }  
@@ -118,12 +131,14 @@ void setup() {
   pinMode(ZaxisEnablePin, INPUT_PULLUP);
 
   attachInterrupt(digitalPinToInterrupt(ENC_A), encoderReadout, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENC_B), encoderReadout, CHANGE);
-  
-  old_AB = (digitalRead(ENC_A) << 1) | digitalRead(ENC_B);  // Startwaarde op basis van echte toestand
+  attachInterrupt(digitalPinToInterrupt(ENC_B), encoderReadout, CHANGE);  
 }
 
 void loop() {
+  bool times10 = !digitalRead(times10Pin);
+  bool times100 = !digitalRead(times100Pin);
+  bool enableY =!digitalRead(YaxisEnablePin);
+  bool enableZ = !digitalRead(ZaxisEnablePin);
   bool feedFWD = !digitalRead(feedFWDPin);
   bool feedREV = !digitalRead(feedREVPin);
   int feedSpeed = analogRead(feedSpeedPot);
@@ -134,28 +149,58 @@ void loop() {
   readDelta = 0;
   interrupts();
 
-  if(rawDelta != 0 || ((feedFWD ^ feedREV) && (millis() - lastFeedTime >= feedInterval))){ 
-    lastFeedTime = millis();
+  if(rawDelta != 0){ 
+    
+    uint8_t encodedDelta = encodeDelta(rawDelta);   
+    Serial1.write(encodedDelta);
+    
+    #ifdef DEBUG
+    Serial.print("Jog delta (raw): ");
+    Serial.println(rawDelta);
+    #endif
 
+  }
+
+  if(prevTimes10 != times10 || prevTimes100 != times100 || prevEnableY != enableY || prevEnableZ != enableZ || (millis() - lastSetupTime >= setupInterval)){
+    uint8_t axis = encodeAxis(times10, times100, enableY, enableZ);
+
+    prevTimes10 = times10;
+    prevTimes100 = times100;
+    prevEnableY = enableY;
+    prevEnableZ = enableZ;
+    lastSetupTime = millis();
+
+    Serial1.write(axis);
+
+    #ifdef DEBUG
+    Serial.print("Sent setup byte: ");
+    Serial.println(axis);
+    #endif
+  } 
+
+  if(prevPressed && !(feedFWD || feedREV)){
+    prevPressed = false;
+    Serial1.write(0b10000000);
+  }
+
+  if((feedFWD ^ feedREV) && (millis() - lastFeedTime >= feedInterval)){
+    prevPressed = true;
+    lastFeedTime = millis();
+    
     if(speedAmount > 0){
       feedSpeed = sumFeedSpeed/speedAmount;
     }
     sumFeedSpeed = 0;
     speedAmount = 0;
 
-    uint8_t encodedDelta = encodeDelta(rawDelta, !digitalRead(times10Pin), !digitalRead(times100Pin), !digitalRead(YaxisEnablePin), !digitalRead(ZaxisEnablePin));   
-    Serial1.write(encodedDelta);
     uint8_t feedByte = encodeFeed(feedFWD, feedREV, feedSpeed);
     Serial1.write(feedByte);
 
     #ifdef DEBUG
-    Serial.print("Jog delta (raw): ");
-    Serial.print(rawDelta);
-    Serial.print(" | Feed speed: ");
+    Serial.print("Feed speed: ");
     Serial.println(feedSpeed);
     #endif
-
-  } else if(speedAmount < 150){
+  } else if(speedAmount < 100){
     speedAmount++;
     sumFeedSpeed += feedSpeed;
   } 
--- receiver_pendant/receiver_pendant.ino
+++ receiver_pendant/receiver_pendant.ino
@@ -1,8 +1,8 @@
 /*
-Project: CNC Pendant, rudamentary receiver side
+Project: CNC Pendant, rudimentary receiver side
 Description: Receives jog and feed bytes and decodes them and adds the values to axes, only for jog, feedrate shown seperately
 Author: Matteo https://github.com/BigLMatt
-Date last altered: 01/07/2025
+Date last altered: 06/07/2025
 
 Receiver is arduino mega
 Hook up RX of receiver to TX of sender and also connect both GND to eachother
@@ -10,11 +10,16 @@ Baudrate is 115200 but higher is better to avoid skipping jog data because delta
 Expandable to 1 extra speed factor and 1 extra axis
 */
 
-int x = 0;
-int y = 0;
-int z = 0;
+static int x = 0;
+static int y = 0;
+static int z = 0;
+bool enableY = false;
+bool enableZ = false;
+int factor = 1;
+int feedSpeed = 0;
 
-// Allows printing out of axes and their
+
+// Allows printing out of axes and their states
 #define DEBUG
 
 void setup() {
@@ -24,55 +29,73 @@ void setup() {
 void loop() {
   static int state = 0;
   static uint8_t buffer[1];
+
+  static bool FWD = false;
+  static bool REV = false;
  
-  while (Serial.available() >=2) {
-    uint8_t jogByte = Serial.read();
-    uint8_t feedByte = Serial.read();
+  while (Serial.available()) {
+    uint8_t encodedByte = Serial.read();    
 
-    // Decoding byte with jog information
-    decodeJog(jogByte, x, y, z);
-
-    bool FWD, REV;
-    int feedSpeed;
-
-    decodeFeed(feedByte, FWD, REV, feedSpeed);
+    // Check which byte was sent
+    if ((encodedByte & 0b10000000) == 0b10000000) {
+      decodeFeed(encodedByte, FWD, REV, feedSpeed);
+    } else if ((encodedByte & 0b11000000) == 0b01000000) {
+      decodeJog(encodedByte, enableY, enableZ, factor, x, y, z);
+    } else if ((encodedByte & 0b11000000) == 0b00000000) {
+      decodeAxis(encodedByte, factor, enableY, enableZ);
+    }   
 
     #ifdef DEBUG
-    Serial.print("X: ");
+    Serial.print(encodedByte);
+    Serial.print("   X: ");
     Serial.print(x);
     Serial.print("  Y: ");
     Serial.print(y);
     Serial.print("  Z: ");
     Serial.print(z);
     Serial.print(" | Direction: ");
-    Serial.print(FWD ? "FWD" : "REV");
+    if (FWD) Serial.print("FWD");
+    else if (REV) Serial.print("REV");
+    else Serial.print("NONE");
     Serial.print(" | Feedrate: ");
     Serial.println(feedSpeed);
     #endif
   }
 }
 
-void decodeFeed(uint8_t feedByte, bool &isFWD, bool &isREV, int &feedrate) {
-  isREV = feedByte & 0b10000000;
-  isFWD = !isREV;
+void decodeFeed(uint8_t feedByte, bool &isFWD, bool &isREV, int &feedSpeed) {
+  if ((feedByte & 0b01111111) == 0) {
+    isFWD = false;
+    isREV = false;
+    feedSpeed = 0;
+    return;
+  }
 
-  feedrate = feedByte & 0b01111111;
+  isREV = (feedByte & 0b01000000) != 0;
+  isFWD = !isREV;
+  feedSpeed = feedByte & 0b00111111;
 }
 
-void decodeJog(uint8_t jogByte, int &x, int &y, int &z){
-  bool sign = jogByte & 0b10000000;
-  uint8_t scaleBits = (jogByte >> 5) & 0b11;
-  uint8_t axis = (jogByte >> 3) & 0b11;
-  uint8_t magnitude = jogByte & 0b00000111;
-
-  int factor = 1;
-  if (scaleBits == 0b01) factor = 10;
-  else if (scaleBits == 0b10) factor = 100;
+void decodeJog(uint8_t jogByte, bool &enableY, bool &enableZ, int &factor, int &x, int &y, int &z){
+  bool sign = jogByte & 0b00100000;
+  uint8_t magnitude = jogByte & 0b00011111;
 
   int delta = magnitude * factor;
   if (sign) delta = -delta;
 
-  if (axis == 0b01) y += delta;
-  else if (axis == 0b10) z += delta;
+  if (enableY) y += delta;
+  else if (enableZ) z += delta;
   else x += delta;
+}
+
+void decodeAxis(uint8_t axisByte, int &factor, bool &enableY, bool &enableZ){
+  uint8_t scaleBits = (axisByte & 0b00110000) >> 4 ;
+  uint8_t axisBits = (axisByte & 0b00001100) >> 2;
+
+  if (scaleBits == 0b01) factor = 10;
+  else if (scaleBits == 0b10) factor = 100;
+  else factor = 1;
+
+  enableY = (axisBits == 0b01);
+  enableZ = (axisBits == 0b10);
 }
